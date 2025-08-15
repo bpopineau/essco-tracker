4849e6217db7010ff4da46d7b07cbcedefe307ea
bpopineau <popineau21@gmail.com>
2025-08-15 13:56:14 -0700

    chore(eslint): ignore underscore-prefixed unused vars

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+﻿import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  { ignores: ["dist/**", "build/**", "node_modules/**", "**/*.min.js"] },
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,mjs}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser,
+    },
+    rules: {
+      "no-unused-vars": ["warn", {
+        argsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+        varsIgnorePattern: "^_"          // <-- add this
+      }],
+      "no-undef": "error",
+      "no-empty": ["error", { allowEmptyCatch: true }]
+    },
+  },
+];
